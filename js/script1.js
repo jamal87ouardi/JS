00f551ccc4a52ac832cf811378c740bd18eec65d
unknown <ouardijamal87@gmail.com>
2024-02-22 12:24:48 +0100

    msg

---
+++ js/script1.js
@@ -0,0 +1,11 @@
+function calculer_somme() {
+
+    var a = parseInt(document.getElementById("a").value);
+    var b = parseInt(document.getElementById("b").value);
+
+    var c = a+b;
+
+    document.getElementById("resultat").innerHTML = c;
+
+  
+}
