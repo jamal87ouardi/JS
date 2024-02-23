8f25290e136f2a5a0f8da5e771460676263952c1
unknown <ouardijamal87@gmail.com>
2024-02-23 11:56:50 +0100

    msg

---
+++ js/script4.js
@@ -0,0 +1,45 @@
+function calculer_prix(){
+
+    const $dis=document.getElementById('nemberr').value;
+
+    var prix;
+
+
+    
+
+    if(document.getElementById("Normal").checked==true)  {
+          if($dis<=10){
+            prix=50
+          }
+
+          else{
+            
+                prix=70
+              
+          }
+
+    }
+
+    else{
+        if($dis<=10){
+            prix=80
+          }
+
+          else{
+            if($dis=>10){
+                prix=100
+              }
+          }
+    }
+
+    document.getElementById("prix").innerHTML=prix;
+
+}
+
+
+function clear_input(){
+    document.getElementById("nemberr").value = "";
+    document.getElementById("prix").innerHTML="0";
+
+
+}
